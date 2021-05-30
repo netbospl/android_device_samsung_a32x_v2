47a71cc3f03213024d400762a5e80aa319ab155e
Piotr Matela <piotrekmat@protonmail.com>
2021-05-30 20:54:30 +0100

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := device/samsung/a32x
 
 include $(CLEAR_VARS)
 
@@ -7,6 +7,6 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 # include the non-open-source counterpart to this file
 # -include vendor/samsung/a32x/AndroidBoardVendor.mk
 
-ifeq ($(TARGET_DEVICE), a32x)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
+# ifeq ($(TARGET_DEVICE), a32x)
+# include $(call all-subdir-makefiles,$(LOCAL_PATH))
+# endif
