51486b133c2234fa1ab8ce05e1233912d5a1ee2d
Piotr Matela <39229923+netbospl@users.noreply.github.com>
2021-05-30 16:59:45 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := a32x
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a32x/device_a32x.mk)
+$(call inherit-product, device/samsung/a32x/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a32x
