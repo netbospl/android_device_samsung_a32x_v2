548b950627c84bda08db1d4cf76edfc6b6c0f999
Piotr Matela <39229923+netbospl@users.noreply.github.com>
2021-05-30 17:36:12 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,11 +1,17 @@
 # Release name
 PRODUCT_RELEASE_NAME := a32x
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from a32x device
+$(call inherit-product, device/samsung/a32x/device.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/a32x/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a32x
