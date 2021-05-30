b9c98aee2c6113c5f633c969015ace60903fb0fa
Piotr Matela <39229923+netbospl@users.noreply.github.com>
2021-05-30 17:36:16 +0100

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,11 +1,17 @@
 # Release name
 PRODUCT_RELEASE_NAME := a32x
 
-# Inherit some common CM stuff.
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a32x/device_a32x.mk)
+$(call inherit-product, device/samsung/a32x/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a32x
