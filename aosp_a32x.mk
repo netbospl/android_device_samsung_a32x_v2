2f6a95a566da5733dce16de94fa5c813449cc71c
Piotr Matela <piotrekmat@protonmail.com>
2021-05-31 02:18:28 +0100

    Update aosp_a32x.mk

--- aosp_a32x.mk
+++ aosp_a32x.mk
@@ -1,7 +1,7 @@
 # Inherit from the common Open Source product configuration
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64.mk)
 
 $(call inherit-product, device/samsung/a32x/device.mk)
 
