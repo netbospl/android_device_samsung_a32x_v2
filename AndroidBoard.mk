7d811f91dc5d4176d85e2d03d47f6510cc026c2f
Piotr Matela <piotrekmat@protonmail.com>
2021-06-01 02:13:03 +0100

    Adopting to Lineage
    At least a try to do so.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,11 +2,12 @@ LOCAL_PATH := device/samsung/a32x
 
 include $(CLEAR_VARS)
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+# ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 # -include vendor/samsung/a32x/AndroidBoardVendor.mk
+-include vendor/samsung/a32x/BoardConfigVendor.mk
 
-# ifeq ($(TARGET_DEVICE), a32x)
-# include $(call all-subdir-makefiles,$(LOCAL_PATH))
-# endif
+ ifeq ($(TARGET_DEVICE), a32x)
+ include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ endif
